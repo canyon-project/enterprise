7bd9cc650eb6037697a250376abb04c4ae27f764
Allen Zhang (张涛) <wr_zhang25@163.com>
2024-08-13 20:31:43 +0800

    upd

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -37,7 +37,7 @@ export default function Login() {
             autoComplete="off"
           >
             <Form.Item<FieldType>
-              label="email"
+              label="Email"
               name="email"
               rules={[{ required: true, message: "Please input your email!" }]}
             >
